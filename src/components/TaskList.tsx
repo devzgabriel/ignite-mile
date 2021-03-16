a96ea172cb514288d868fa9e70b5e3c0a7e25a98
Gabriel Silva <devzgabriel@gmail.com>
2021-03-15 22:53:55 -0300

    refactoring :rocket:

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,24 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
-    if (newTaskTitle !== '') {
-      const newTask = {
-        id: Math.random() * 3,
-        title: newTaskTitle,
-        isComplete: false,
-      }
-      setTasks([...tasks, newTask])
+    if (!newTaskTitle) return
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
     }
-    return
+    setTasks((prevState) => [...prevState, newTask])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-    let taskToChange = tasks.find((task) => task.id === id)
-    if (!taskToChange) return
-    const taskFiltered = tasks.filter((task) => task.id !== id)
-    taskToChange.isComplete = !taskToChange?.isComplete
-    setTasks([...taskFiltered, taskToChange])
+    // let taskToChange = tasks.find((task) => task.id === id)
+    // if (!taskToChange) return
+    // const taskFiltered = tasks.filter((task) => task.id !== id)
+    // taskToChange.isComplete = !taskToChange?.isComplete
+    // setTasks([...taskFiltered, taskToChange])
+
+    const newTasks = tasks.map((task) =>
+      task.id === id
+        ? {
+            ...task,
+            isComplete: !task.isComplete,
+          }
+        : task
+    )
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
